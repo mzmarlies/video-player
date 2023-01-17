e0bccb0f9f3722b2da42fa7db0a29e5e37a37db7
Marlies Zimmer <marlies.zimmer@rakuten.com>
2023-01-17 10:16:03 -0700

    finished exercise

---
+++ scripts.js
@@ -0,0 +1,87 @@
+// Get our elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Build our functions
+function togglePlay() {
+    // 'paused' is a property that lives on the video (there is no 'playing')
+    // if(video.paused) {
+    //     video.play();
+    // } else {
+    //     video.pause();
+    // }
+    // another option of doing what's above:
+    const method = video.paused ? 'play' : 'pause';
+    video[method](); 
+}
+
+function updateButton() {
+    // 'this' here is bound to the video itself"
+    const icon = this.paused ? '►' : '❚ ❚';
+    console.log(icon)
+    toggle.textContent = icon;
+}
+
+
+function skip() {
+    console.log(this.dataset.skip)
+    // this is a string, so we wrap it in a parseFloat to convert it to a true number
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+    console.log(this.name);
+    console.log(this.value);
+}
+
+function handleProgress() {
+    // 'currentTime' and 'duration' are just properties on the video
+    // we mulptiply it by 100 to avoid a decimal (0.5) and instead get 50% 
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+// Hook up the event listeners
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+// we'll listen for the video to emit an event called timeupdate
+// when that happens, we will run handleProgress
+video.addEventListener('timeupdate', handleProgress);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+// listen for a change on the specific sliders
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+
+progress.addEventListener('click', scrub);
+
+// progress.addEventListener('mousemove', () => {
+//     if(mousedown) {
+//         scrub();
+//     }
+// });
+// could also do it like so:
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+// BONUS:
+// try to make the video go fullscreen
